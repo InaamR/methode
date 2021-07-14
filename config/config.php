1deb8151550c7f3fd7f14052302a60daa60f2ef4
InaamR <inaam.rach@gmail.com>
2021-07-14 16:36:06 +0200

    Modif

--- config/config.php
+++ config/config.php
@@ -5,5 +5,5 @@ $PARAM_utilisateur = 'root';
 $PARAM_mot_passe   = ''; 
 $PARAM_nom_site    = 'Cube - Methode'; 
 $PARAM_mail_site   = 'Mohamed.INAAMRACHDI@infopro-digital.com'; 
-$PARAM_url_site    = 'http://localhost/METHODE/v_finale/page/module'; 
+$PARAM_url_site    = 'http://localhost/methode/page/module'; 
 ?>
