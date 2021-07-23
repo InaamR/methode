1e5c1e0954ce560a73b7ae31a1841ca876d2beab
InaamR <inaam.rach@gmail.com>
2021-07-23 03:07:52 +0200

    Maj

--- v2/config/function.php
+++ v2/config/function.php
@@ -1401,6 +1401,12 @@ class Admin {
 		$lien = URLSITE.'/administrateur/identite_vehicule/';
 		return $lien;
 	}
+
+	public static function menuchapitre() {
+		$lien = URLSITE.'/administrateur/chapitre/';
+		return $lien;
+	}
+	
 		
 } // Fin de la classe administrateur
 ?>
