e010565c992828851338e0aed9cae4e2ea5d6575
MedZiedJlassi <47533589+MedZiedJlassi@users.noreply.github.com>
2021-08-17 09:32:30 +0100

    Logo

--- v2/assets/js/scripts.js
+++ v2/assets/js/scripts.js
@@ -1,10 +1,27 @@
 (function (window, undefined) {
   'use strict';
+    var logoMini = document.getElementById('brand-logo');
+    var logoBig = document.getElementById('brand-logo-big');
+    
+    document.addEventListener('DOMContentLoaded', ()=>{
+      var elem = document.getElementById('navbar-header');
+      elem.addEventListener('load', (ev)=>{
+        console.log(elem.width);
+      })
+    })
+    
+    
+    
+    
 
+    // function toggleLogo(){
+    //   var newWidth = window.innerWidth;
+    //   if(newWidth)
+    }
   /*
   NOTE:
   ------
   PLACE HERE YOUR OWN JAVASCRIPT CODE IF NEEDED
   WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR JAVASCRIPT CODE PLEASE CONSIDER WRITING YOUR SCRIPT HERE.  */
 
-})(window);
+}(window);
