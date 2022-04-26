70d54afc2ac2f9b111703b775ce2266c67f41dd4
InaamR <83518345+InaamR@users.noreply.github.com>
2022-04-26 14:12:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (preg_match("/config/", $page)) {
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">    
     <meta name="author" content="PIXINVENT">
-    <title>Connexion - <?php echo $PARAM_nom_site; ?></title>
+    <title>Connexionn - <?php echo $PARAM_nom_site; ?></title>
     <link rel="apple-touch-icon" href="http://<?php echo $_SERVER['SERVER_NAME']?>/<?php echo $PARAM_url_non_doc_site; ?>/app-assets/images/ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="http://<?php echo $_SERVER['SERVER_NAME']?>/<?php echo $PARAM_url_non_doc_site; ?>/app-assets/images/ico/favicon-16x16.png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
